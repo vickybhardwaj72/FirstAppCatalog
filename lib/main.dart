b425cd10d8ca00bfda92430cc78a4e736aaecaa1
badvicky72@gmail.com <badvicky72@gmail.com>
2022-06-02 13:03:13 -0400

    day4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_catalog/pages/login_page.dart';
+
+
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +12,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home":(context) => HomePage(),
+        "/login":(context) => LoginPage()
+      },
     ); 
+
   }
 }
